21defdbb402af862e10bcbbb629c482c08002ef5
azizramdan <azizramdan44@gmail.com>
2019-03-17 20:46:14 +0700

    add filter form

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,11 +3,33 @@
 @section("title") Users list @endsection 
 
 @section("content")
+    <div class="container">
     @if(session('status'))
       <div class="alert alert-success">
         {{session('status')}}
       </div>
     @endif 
+    <div class="row">
+      <div class="col-md-6">
+        <form action="{{route('users.index')}}">
+            <div class="input-group mb-3">
+            <input
+              value="{{Request::get('keyword')}}" 
+              name="keyword" 
+              class="form-control col-md-10" 
+              type="text"
+              placeholder="Filter berdasarkan email"/>
+
+                <div class="input-group-append">
+                    <input 
+                    type="submit" 
+                    value="Filter" 
+                    class="btn btn-primary">
+                </div>
+            </div>
+        </form>
+        </div>
+    </div>
     <table class="table table-bordered">
       <thead>
         <tr>
@@ -34,11 +56,6 @@
             </td>
             <td>
             <a class="btn btn-info text-white btn-sm" href="{{route('users.edit', ['id'=>$user->id])}}">Edit</a>
-            @if(session('status'))
-              <div class="alert alert-success">
-                {{session('status')}}
-              </div>
-            @endif 
             <form 
             onsubmit="return confirm('Delete this user permanently?')" 
             class="d-inline" 
@@ -65,5 +82,5 @@
         @endforeach 
       </tbody>
     </table>
-
+    </div>
 @endsection
