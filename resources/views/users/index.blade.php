ecb7196104d15e50a6326527b14f39d9fbc7e997
azizramdan <azizramdan44@gmail.com>
2019-03-17 20:31:02 +0700

    tambah tombol delete di list user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,7 +3,11 @@
 @section("title") Users list @endsection 
 
 @section("content")
-
+    @if(session('status'))
+      <div class="alert alert-success">
+        {{session('status')}}
+      </div>
+    @endif 
     <table class="table table-bordered">
       <thead>
         <tr>
@@ -30,6 +34,29 @@
             </td>
             <td>
             <a class="btn btn-info text-white btn-sm" href="{{route('users.edit', ['id'=>$user->id])}}">Edit</a>
+            @if(session('status'))
+              <div class="alert alert-success">
+                {{session('status')}}
+              </div>
+            @endif 
+            <form 
+            onsubmit="return confirm('Delete this user permanently?')" 
+            class="d-inline" 
+            action="{{route('users.destroy', ['id' => $user->id ])}}" 
+            method="POST">
+
+              @csrf
+
+              <input 
+                type="hidden" 
+                name="_method" 
+                value="DELETE">
+
+              <input 
+                type="submit" 
+                value="Delete" 
+                class="btn btn-danger btn-sm">
+            </form>
             </td>
           </tr>
         @endforeach 
