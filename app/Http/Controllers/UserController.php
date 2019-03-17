ee051b314cf64eb8c7a58bce12c82f1747eea6c2
azizramdan <azizramdan44@gmail.com>
2019-03-17 16:01:42 +0700

    users resource controller action create & store

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newUser = new \App\User;
+        $newUser->name = $request->get('name');
+        $newUser->username = $request->get('username');
+        $newUser->roles = json_encode($request->get('roles'));
+        $newUser->name = $request->get('name');
+        $newUser->address = $request->get('address');
+        $newUser->phone = $request->get('phone');
+        $newUser->email = $request->get('email');
+        $newUser->password = \Hash::make($request->get('password'));
+
+        if($request->file('avatar')){
+            $file = $request->file('avatar')->store('avatars', 'public');
+        
+            $newUser->avatar = $file;
+        }
+        $newUser->save();
+        return redirect()->route('users.create')->with('status', 'User successfully created.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
